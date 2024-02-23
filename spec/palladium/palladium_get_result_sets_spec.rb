d58028a5c83471fe9166589cfd333b39cdcfbd59
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-02-23 10:39:21 +0200

    Fix more rubocop

--- spec/palladium/palladium_get_result_sets_spec.rb
+++ spec/palladium/palladium_get_result_sets_spec.rb
@@ -1,23 +1,68 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'palladium'
 
 RSpec.describe Palladium do
   describe '#get_result_sets' do
-    it 'sends a request to get result sets' do
-      http_double = double('Net::HTTP')
+    let(:http_double) { instance_double(Net::HTTP) }
+    let(:response_body) do
+      '{"run": {"id": 123}, "product": {"id": 456}, "plan": {"id": 789}, "result_sets": [{"id": 999}]}'
+    end
+
+    before do
       allow(Net::HTTP).to receive(:new).and_return(http_double)
       allow(http_double).to receive(:use_ssl=)
-      allow(http_double).to receive(:request).and_return(double('response', body: '{"run": {"id": 123}, "product": {"id": 456}, "plan": {"id": 789}, "result_sets": [{"id": 999}]}'))
+      allow(http_double).to receive(:request).and_return(instance_double(response, body: response_body))
+    end
 
-      palladium = Palladium.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan', run: 'SampleRun', token: 'sample_token')
+    it 'sends a request to get result sets' do
+      palladium = described_class.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan',
+                                      run: 'SampleRun', token: 'sample_token')
       allow(palladium).to receive(:ssl_connection?).and_return(true)
 
       result_sets = palladium.get_result_sets('passed')
 
       expect(result_sets).to be_an(Array)
-      expect(result_sets).to include({"id" => 999})
+    end
+
+    it 'includes the expected result set' do
+      palladium = described_class.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan',
+                                      run: 'SampleRun', token: 'sample_token')
+      allow(palladium).to receive(:ssl_connection?).and_return(true)
+
+      result_sets = palladium.get_result_sets('passed')
+
+      expect(result_sets).to include({ 'id' => 999 })
+    end
+
+    it 'sets the run id' do
+      palladium = described_class.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan',
+                                      run: 'SampleRun', token: 'sample_token')
+      allow(palladium).to receive(:ssl_connection?).and_return(true)
+
+      palladium.get_result_sets('passed')
+
       expect(palladium.instance_variable_get(:@run_id)).to eq(123)
+    end
+
+    it 'sets the product id' do
+      palladium = described_class.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan',
+                                      run: 'SampleRun', token: 'sample_token')
+      allow(palladium).to receive(:ssl_connection?).and_return(true)
+
+      palladium.get_result_sets('passed')
+
       expect(palladium.instance_variable_get(:@product_id)).to eq(456)
+    end
+
+    it 'sets the plan id' do
+      palladium = described_class.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan',
+                                      run: 'SampleRun', token: 'sample_token')
+      allow(palladium).to receive(:ssl_connection?).and_return(true)
+
+      palladium.get_result_sets('passed')
+
       expect(palladium.instance_variable_get(:@plan_id)).to eq(789)
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,10 @@ def update_instance_vars(palladium)
 end
 
 def create_http_double
-  http_double = instance_double('Net::HTTP')
+  http_double = instance_double(Net::HTTP)
   allow(Net::HTTP).to receive(:new).and_return(http_double)
   allow(http_double).to receive(:use_ssl=)
   body = '{"other_data": {"run_id": 123, "product_id": 456, "plan_id": 789, "result_set_id": 999}}'
-  allow(http_double).to receive(:request).and_return(instance_double('response',
-                                                            body: body))
+  allow(http_double).to receive(:request).and_return(instance_double(response,
+                                                                     body: body))
 end
