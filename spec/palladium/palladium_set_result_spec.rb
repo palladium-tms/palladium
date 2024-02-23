1ae15df72da45a123749767c82540f167d48bf9f
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-02-23 10:35:54 +0200

    Fix another file

--- spec/palladium/palladium_set_result_spec.rb
+++ spec/palladium/palladium_set_result_spec.rb
@@ -1,29 +1,47 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'palladium'
 
 RSpec.describe Palladium do
+  let(:options) do
+    {
+      name: 'Sample Result',
+      description: 'This is a sample result',
+      status: 'passed'
+    }
+  end
+
+  let(:palladium) { described_class.new(host: 'example.com') }
+
+  let(:result) { palladium.set_result(options) }
+
+  before do
+    create_http_double
+    allow(palladium).to receive(:instance_variable_get).with(:@run_id).and_return(123)
+    allow(palladium).to receive(:instance_variable_get).with(:@product_id).and_return(456)
+    allow(palladium).to receive(:instance_variable_get).with(:@plan_id).and_return(789)
+    allow(palladium).to receive(:instance_variable_get).with(:@result_set_id).and_return(999)
+  end
+
   describe '#set_result' do
-    it 'sends a request to set the result' do
-      options = {
-        name: 'Sample Result',
-        description: 'This is a sample result',
-        status: 'passed'
-      }
-
-      http_double = double('Net::HTTP')
-      allow(Net::HTTP).to receive(:new).and_return(http_double)
-      allow(http_double).to receive(:use_ssl=)
-      allow(http_double).to receive(:request).and_return(double('response', body: '{"other_data": {"run_id": 123, "product_id": 456, "plan_id": 789, "result_set_id": 999}}'))
-
-      palladium = Palladium.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan', run: 'SampleRun', token: 'sample_token')
-      allow(palladium).to receive(:ssl_connection?).and_return(true)
-
-      result = palladium.set_result(options)
-
+    it 'result includes request data' do
       expect(result).to include('other_data')
+    end
+
+    it 'result run id is correct' do
       expect(palladium.instance_variable_get(:@run_id)).to eq(123)
+    end
+
+    it 'result product id is correct' do
       expect(palladium.instance_variable_get(:@product_id)).to eq(456)
+    end
+
+    it 'result plan id is correct' do
       expect(palladium.instance_variable_get(:@plan_id)).to eq(789)
+    end
+
+    it 'result set id is correct' do
       expect(palladium.instance_variable_get(:@result_set_id)).to eq(999)
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 # Method to update instance variables of Palladium class
 def update_instance_vars(palladium)
   palladium.instance_variable_set(:@product_id, 456)
   palladium.instance_variable_set(:@plan_id, 789)
   palladium.instance_variable_set(:@run_id, 123)
   palladium.instance_variable_set(:@result_set_id, 999)
+end
+
+def create_http_double
+  http_double = instance_double('Net::HTTP')
+  allow(Net::HTTP).to receive(:new).and_return(http_double)
+  allow(http_double).to receive(:use_ssl=)
+  body = '{"other_data": {"run_id": 123, "product_id": 456, "plan_id": 789, "result_set_id": 999}}'
+  allow(http_double).to receive(:request).and_return(instance_double('response',
+                                                            body: body))
 end
