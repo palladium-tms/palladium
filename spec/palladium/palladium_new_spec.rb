d507e6a178af960da1fb921839c3508a167cd9d2
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-02-23 10:18:18 +0200

    Reduce rubocop metric

--- spec/palladium/palladium_new_spec.rb
+++ spec/palladium/palladium_new_spec.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'palladium'
 
 RSpec.describe Palladium do
   describe '.new' do
     it 'creates a new Palladium instance' do
-      palladium = Palladium.new(host: 'example.com', product: 'SampleProduct', plan: 'SamplePlan', run: 'SampleRun', token: 'sample_token')
+      palladium = described_class.new(host: 'example.com',
+                                      token: 'sample_token')
       expect(palladium).to be_an_instance_of(Palladium::Palladium)
     end
   end
